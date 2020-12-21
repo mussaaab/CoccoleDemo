6aa6d29e5c54ae838855b0e642df50cb5154dc93
mussab_yaqoob <yaqoobmussab@gmail.com>
2020-12-21 15:32:28 +0500

    resolved scroll issue and add calendar modal

--- src/Screens/Auth/Register.js
+++ src/Screens/Auth/Register.js
@@ -1,115 +1,266 @@
-import React, { Component } from 'react'
-import { Text, View, Dimensions, TouchableOpacity, Image, TextInput, ScrollView } from 'react-native'
-import { Images, Colors } from '../../Config'
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import { Images, Colors } from '../../Config';
+import { Header } from '../../Components';
 
-// Icons
-import Entypo from 'react-native-vector-icons/Entypo'
-
-const { width, height } = Dimensions.get('window')
+const { width, height } = Dimensions.get('screen');
 
 export class Register extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, backgroundColor: Colors.Blue_Chill }}>
-                {/* <ScrollView> */}
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: Colors.Blue_Chill }}>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Header navigation={this.props.navigation} />
+            <View
+              style={{
+                width: width * 0.9,
+                height: height - height * 0.2,
+                backgroundColor: Colors.white,
+                borderTopLeftRadius: 20,
+                borderTopRightRadius: 20,
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: 'center',
+                  paddingTop: 20,
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  color: '#354953',
+                }}
+              >
+                Registrati aI Club Pampers
+              </Text>
+              <Text style={{ textAlign: 'center', paddingTop: 10 }}>
+                Usa i tuoi social
+              </Text>
 
-                    <View style={{ justifyContent: 'center' }}>
+              <View
+                style={{
+                  height: height * 0.12,
+                  width: width * 0.7,
+                  alignSelf: 'center',
+                  marginTop: 10,
+                  flexDirection: 'row',
+                }}
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <TouchableOpacity>
+                    <Image
+                      source={Images.apple_icon}
+                      style={{
+                        width: width * 0.15,
+                        height: height * 0.06,
+                        resizeMode: 'contain',
+                      }}
+                    />
+                  </TouchableOpacity>
+                  <Text>Apple</Text>
+                </View>
 
-                        <View style={{ height: height * 0.2, justifyContent: 'center', alignItems: 'center' }}>
-                            <Image source={Images.Pamper_logo} style={{
-                                width: width * 0.4,
-                                height: height * 0.08,
-                                resizeMode: 'contain',
-                                // backgroundColor: 'pink'
-                            }} />
-                            <Text style={{ color: Colors.white }}>Con amore si cambia</Text>
-                        </View>
+                <View style={{ flex: 1 }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <TouchableOpacity>
+                      <Image
+                        source={Images.facebook_icon}
+                        style={{
+                          width: width * 0.15,
+                          height: height * 0.06,
+                          resizeMode: 'contain',
+                        }}
+                      />
+                    </TouchableOpacity>
 
-                        <View style={{ flex: 1, position: 'absolute', marginHorizontal: 10 }}>
-                            <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={{ width: height * 0.07, height: height * 0.07, alignItems: 'center', justifyContent: 'center', backgroundColor: '#3cc9cf', borderRadius: 100, elevation: 5 }}>
-                                <Entypo name="chevron-left" size={32} color={Colors.white} />
-                            </TouchableOpacity>
-                        </View>
+                    <Text>Facebook</Text>
+                  </View>
+                </View>
 
-                    </View>
+                <View style={{ flex: 1 }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <TouchableOpacity>
+                      <Image
+                        source={Images.google_icon}
+                        style={{
+                          width: width * 0.15,
+                          height: height * 0.06,
+                          resizeMode: 'contain',
+                        }}
+                      />
+                    </TouchableOpacity>
 
+                    <Text>Google</Text>
+                  </View>
+                </View>
+              </View>
 
+              <View
+                style={{
+                  flexDirection: 'row',
+                  width: width * 0.8,
+                  alignSelf: 'center',
+                }}
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    borderBottomWidth: 1,
+                    borderBottomColor: Colors.grey,
+                  }}
+                />
 
-                    <View style={{ width: width * 0.9, flex: 1, alignSelf: 'center', backgroundColor: Colors.white, borderTopLeftRadius: 20, borderTopRightRadius: 20 }}>
-                        <Text style={{ textAlign: 'center', paddingTop: 20, fontSize: 20, fontWeight: 'bold', color: "#354953" }}>Registrati aI Club Pampers</Text>
-                        <Text style={{ textAlign: 'center', paddingTop: 10 }}>Usa i tuoi social</Text>
+                <View style={{ marginHorizontal: 5 }}>
+                  <Text>oppure</Text>
+                </View>
 
-                        <View style={{ height: height * 0.12, width: width * 0.7, alignSelf: 'center', marginTop: 10, flexDirection: 'row', }}>
-                            <View style={{ flex: 1, alignItems: "center", justifyContent: 'center' }}>
-                                <TouchableOpacity >
-                                    <Image source={Images.apple_icon} style={{ width: width * 0.15, height: height * 0.06, resizeMode: 'contain', }} />
-                                </TouchableOpacity>
-                                <Text>Apple</Text>
-                            </View>
+                <View
+                  style={{
+                    flex: 1,
+                    borderBottomWidth: 1,
+                    borderBottomColor: Colors.grey,
+                  }}
+                />
+              </View>
 
-                            <View style={{ flex: 1 }}>
-                                <View style={{ flex: 1, alignItems: "center", justifyContent: 'center' }}>
-                                    <TouchableOpacity>
-                                        <Image source={Images.facebook_icon} style={{ width: width * 0.15, height: height * 0.06, resizeMode: 'contain', }} />
-                                    </TouchableOpacity>
+              <View
+                style={{
+                  width: width * 0.8,
+                  alignSelf: 'center',
+                  paddingVertical: 10,
+                  marginTop: 15,
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, fontWeight: 'bold', color: '#354953' }}
+                >
+                  E-mail
+                </Text>
 
-                                    <Text>Facebook</Text>
-                                </View>
+                <TextInput
+                  placeholder="Inserisci il tuo indirizzo e-mail"
+                  placeholderTextColor={Colors.silver}
+                  style={{
+                    width: width * 0.8,
+                    alignSelf: 'center',
+                    marginTop: 10,
+                    borderRadius: 100,
+                    paddingLeft: 20,
+                    borderWidth: 1,
+                    borderColor: Colors.grey,
+                    color: Colors.silver,
+                  }}
+                />
+              </View>
 
+              <View
+                style={{
+                  width: width * 0.8,
+                  alignSelf: 'center',
+                  paddingVertical: 10,
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, fontWeight: 'bold', color: '#354953' }}
+                >
+                  Password
+                </Text>
 
-                            </View>
+                <TextInput
+                  placeholder="Inserisci la tua password"
+                  placeholderTextColor={Colors.silver}
+                  style={{
+                    width: width * 0.8,
+                    alignSelf: 'center',
+                    marginTop: 10,
+                    borderRadius: 100,
+                    paddingLeft: 20,
+                    borderWidth: 1,
+                    borderColor: Colors.grey,
+                    color: Colors.silver,
+                  }}
+                />
+              </View>
 
-                            <View style={{ flex: 1, }}>
-                                <View style={{ flex: 1, alignItems: "center", justifyContent: 'center' }}>
-                                    <TouchableOpacity >
-                                        <Image source={Images.google_icon} style={{ width: width * 0.15, height: height * 0.06, resizeMode: 'contain', }} />
-                                    </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() =>
+                  this.props.navigation.navigate('SelectDateOfBirth')
+                }
+                style={{
+                  width: width * 0.8,
+                  alignSelf: 'center',
+                  backgroundColor: Colors.silver,
+                  borderWidth: 1,
+                  borderColor: Colors.grey,
+                  borderRadius: 100,
+                  paddingVertical: 15,
+                  marginTop: 15,
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: 'center',
+                    color: Colors.white,
+                    fontWeight: 'bold',
+                  }}
+                >
+                  Procedi
+                </Text>
+              </TouchableOpacity>
 
-                                    <Text>Google</Text>
-                                </View>
-                            </View>
-                        </View>
+              <View
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  flexDirection: 'row',
+                  backgroundColor: Colors.white,
+                  flex: 1,
+                }}
+              >
+                <Text style={{ fontSize: 16 }}>Hai già un account? </Text>
 
-                        <View style={{ flexDirection: 'row', width: width * 0.8, alignSelf: 'center' }}>
-                            <View style={{ flex: 1, borderBottomWidth: 1, borderBottomColor: Colors.grey }} />
-
-                            <View style={{ marginHorizontal: 5 }}><Text>oppure</Text></View>
-
-                            <View style={{ flex: 1, borderBottomWidth: 1, borderBottomColor: Colors.grey }} />
-                        </View>
-
-
-
-                        <View style={{ width: width * 0.8, alignSelf: 'center', paddingVertical: 10, marginTop: 15 }}>
-                            <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#354953' }}>E-mail</Text>
-
-                            <TextInput placeholder="Inserisci il tuo indirizzo e-mail" placeholderTextColor={Colors.silver} style={{ width: width * 0.8, alignSelf: 'center', marginTop: 10, borderRadius: 100, paddingLeft: 20, borderWidth: 1, borderColor: Colors.grey, color: Colors.silver }} />
-                        </View>
-
-                        <View style={{ width: width * 0.8, alignSelf: 'center', paddingVertical: 10, }}>
-                            <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#354953' }}>Password</Text>
-
-                            <TextInput placeholder="Inserisci la tua password" placeholderTextColor={Colors.silver} style={{ width: width * 0.8, alignSelf: 'center', marginTop: 10, borderRadius: 100, paddingLeft: 20, borderWidth: 1, borderColor: Colors.grey, color: Colors.silver }} />
-                        </View>
-
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate("SelectDateOfBirth")} style={{ width: width * 0.8, alignSelf: 'center', backgroundColor: Colors.silver, borderWidth: 1, borderColor: Colors.grey, borderRadius: 100, paddingVertical: 15, marginTop: 15 }}>
-                            <Text style={{ textAlign: 'center', color: Colors.white, fontWeight: 'bold' }}>Procedi</Text>
-                        </TouchableOpacity>
-
-                        <View style={{ flex: 1, alignItems: 'center', alignItems: 'flex-end', justifyContent: 'center', flexDirection: 'row' }}>
-                            <Text style={{ paddingVertical: 10, fontSize: 16 }}>Hai già un account? {' '}</Text>
-
-                            <TouchableOpacity>
-                                <Text style={{ paddingVertical: 10, color: Colors.fountain_blue, fontSize: 16 }}>Accedi</Text>
-                            </TouchableOpacity>
-                        </View>
-
-
-                    </View>
-                {/* </ScrollView> */}
+                <TouchableOpacity>
+                  <Text
+                    style={{
+                      color: Colors.fountain_blue,
+                      fontSize: 16,
+                    }}
+                  >
+                    Accedi
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
-        )
-    }
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
 }
 
-export default Register
+export default Register;
--- src/Screens/Auth/SelectDateOfBirth.js
+++ src/Screens/Auth/SelectDateOfBirth.js
@@ -1,61 +1,201 @@
-import React, { Component } from 'react'
-import { Text, View, Dimensions, TouchableOpacity, Image } from 'react-native'
-import { ProgressBar } from '@react-native-community/progress-bar-android';
-import { Header } from '../../Components/Header';
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  Modal,
+} from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import { Header, ProgressBar } from '../../Components';
+// import { Header } from '../../Components';
 import { Images, Colors } from '../../Config';
+import { Months } from '../../Config/Api';
 
-// Icons
-import Entypo from 'react-native-vector-icons/Entypo'
-
-
-
-const { width, height } = Dimensions.get('window')
+const { width, height } = Dimensions.get('window');
 
 export class SelectDateOfBirth extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <Header navigation={this.props.navigation} />
+  state = {
+    date: null,
+    calendar_modal: false,
+  };
+  render() {
+    let date = this.state.date?.split('-');
+    date && console.warn(date[1]);
+    return (
+      <View style={{ flex: 1, backgroundColor: Colors.white }}>
+        <Header navigation={this.props.navigation} />
 
-                <View style={{ flex: 1 }}>
-                    <View style={{ width: width * 0.9, flexDirection: 'row', alignSelf: 'center' }}>
-                        <ProgressBar styleAttr="Horizontal"
-                            indeterminate={false}
-                            progress={0.2}
-                            color={Colors.Blue_Chill}
-                            style={{ width: width * 0.8 }} />
+        <View
+          style={{
+            width: width * 0.9,
+            alignSelf: 'center',
+          }}
+        >
+          <ProgressBar value={0.25} />
+        </View>
 
-                        <TouchableOpacity style={{ width: height * 0.05, height: height * 0.05, borderRadius: 100, alignItems: 'center', justifyContent: 'center', backgroundColor: Colors.sky_blue }}>
-                            <Entypo name="heart-outlined" size={20} color={Colors.deep_sea_green} />
-                        </TouchableOpacity>
-                    </View>
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              width: width * 0.8,
+              alignSelf: 'center',
+              paddingVertical: 15,
+            }}
+          >
+            <Text
+              style={{ fontSize: 20, textAlign: 'center', fontWeight: 'bold' }}
+            >
+              Inserisci la data di nascita del tuo bimbo più piccolo o presunta
+              data del parto
+            </Text>
+          </View>
 
-                    <View style={{ width: width * 0.8, alignSelf: "center", paddingVertical: 15 }}>
-                        <Text style={{ fontSize: 20, textAlign: 'center', fontWeight: 'bold' }}>Inserisci la data di nascita del tuo bimbo più piccolo o presunta data del parto</Text>
-                    </View>
+          <View
+            style={{
+              width: width * 0.8,
+              borderWidth: 1,
+              borderColor: Colors.grey,
+              borderRadius: 100,
+              alignSelf: 'center',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingVertical: 15,
+            }}
+          >
+            <Text style={{ paddingLeft: 15, color: Colors.silver }}>
+              {!this.state.date
+                ? 'Inserisci la data di nascita'
+                : `${date[2]} ${Months[date[1]]} ${date[0]}`}
+            </Text>
 
-                    <View style={{ width: width * 0.8, borderWidth: 1, borderColor: Colors.grey, borderRadius: 100, alignSelf: "center", flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 15 }}>
-                        <Text style={{ paddingLeft: 15, color: Colors.silver }}>Inserisci la data di nascita</Text>
+            <TouchableOpacity
+              onPress={() =>
+                this.setState({
+                  calendar_modal: true,
+                })
+              }
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginHorizontal: 15,
+              }}
+            >
+              <Image
+                source={Images.calendar}
+                style={{
+                  width: height * 0.03,
+                  height: height * 0.03,
+                  resizeMode: 'contain',
+                }}
+              />
+            </TouchableOpacity>
+          </View>
 
-                        <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', marginHorizontal: 15 }}>
-                            <Image source={Images.calendar} style={{ width: height * 0.03, height: height * 0.03, resizeMode: 'contain' }} />
-                        </TouchableOpacity>
-                    </View>
+          <TouchableOpacity
+            style={{ width: width * 0.7, marginTop: 20, alignSelf: 'center' }}
+          >
+            <Text style={{ color: Colors.Blue_Chill }}>
+              Non conosci la data presunta del parto?
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-                    <TouchableOpacity style={{ width: width * 0.7, marginTop: 20, alignSelf: 'center' }}>
-                        <Text style={{ color: Colors.Blue_Chill }}>Non conosci la data presunta del parto?</Text>
-                    </TouchableOpacity>
-                </View>
+        <View
+          style={{
+            marginHorizontal: 15,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate('')}
+            style={{
+              width: width * 0.8,
+              alignSelf: 'center',
+              backgroundColor: this.state.date
+                ? Colors.fountain_blue
+                : Colors.silver,
+              borderWidth: 1,
+              borderColor: Colors.grey,
+              borderRadius: 100,
+              paddingVertical: 15,
+              marginTop: 15,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: 'center',
+                color: Colors.white,
+                fontWeight: 'bold',
+              }}
+            >
+              Procedi
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-                <View style={{ marginHorizontal: 15, alignItems: 'center', justifyContent: 'center' }}>
-
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate("")} style={{ width: width * 0.8, alignSelf: 'center', backgroundColor: Colors.silver, borderWidth: 1, borderColor: Colors.grey, borderRadius: 100, paddingVertical: 15, marginTop: 15 }}>
-                        <Text style={{ textAlign: 'center', color: Colors.white, fontWeight: 'bold' }}>Procedi</Text>
-                    </TouchableOpacity>
-                </View>
+        {/* Calendar Modal */}
+        <Modal visible={this.state.calendar_modal} transparent={true}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'rgba(232, 232, 232,0.7)',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <View
+              style={{
+                width: width * 0.9,
+                backgroundColor: 'red',
+              }}
+            >
+              <Calendar
+                theme={{
+                  backgroundColor: '#ffffff',
+                  calendarBackground: '#ffffff',
+                  arrowColor: Colors.fountain_blue,
+                  selectedDayBackgroundColor: Colors.fountain_blue,
+                  selectedDayTextColor: Colors.white,
+                }}
+                ref={(ref) => (this.calendarRef = ref)}
+                current={new Date()}
+                markedDates={{ [this.state.date]: { selected: true } }}
+                onDayPress={(day) =>
+                  this.setState({ date: day.dateString, calendar_modal: false })
+                }
+                monthFormat={'DD, MM, YYYY'}
+                onMonthChange={(month) => {
+                  console.log('month changed', month);
+                }}
+                firstDay={1}
+                onPressArrowLeft={(subtractMonth) => subtractMonth()}
+                onPressArrowRight={(addMonth) => addMonth()}
+                disableAllTouchEventsForDisabledDays={true}
+                enableSwipeMonths={true}
+                renderHeader={(date) => (
+                  <View>
+                    <Text
+                      style={{
+                        color: Colors.fountain_blue,
+                        fontSize: 18,
+                      }}
+                    >
+                      {Months[new Date(date).getMonth()]}{' '}
+                      {new Date(date).getFullYear()}
+                    </Text>
+                  </View>
+                )}
+              />
             </View>
-        )
-    }
+          </View>
+        </Modal>
+        {/* xxxx Calendar Modal xxxx */}
+      </View>
+    );
+  }
 }
 
-export default SelectDateOfBirth
+export default SelectDateOfBirth;
