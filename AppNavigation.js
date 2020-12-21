65bc90c59dea80fd247c6afb2e7044af8310ac1d
mussab_yaqoob <yaqoobmussab@gmail.com>
2020-12-21 15:03:44 +0500

    added splashLoading

--- AppNavigation.js
+++ AppNavigation.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
 // Screens
-import { RegisterOrLoginScreen, Register, SelectDateOfBirth } from './src/Screens'
+import { RegisterOrLoginScreen, Register, SelectDateOfBirth, SplashLoading } from './src/Screens'
 
 const AuthStack = createStackNavigator();
 
+
+
 const Auth = () => {
     return (
-        <AuthStack.Navigator initialRouteName="RegisterOrLogin">
+        <AuthStack.Navigator initialRouteName="SplashLoading">
+            <AuthStack.Screen name="SplashLoading" component={SplashLoading} options={{ headerShown: false }} />
             <AuthStack.Screen name="RegisterOrLogin" component={RegisterOrLoginScreen} options={{ headerShown: false }} />
             <AuthStack.Screen name="Register" component={Register} options={{ headerShown: false }} />
             <AuthStack.Screen name="SelectDateOfBirth" component={SelectDateOfBirth} options={{ headerShown: false }} />
